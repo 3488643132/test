04407d279e96fc1f786044e6b9709d566fcbcc12
Kdz <64460590+Kongdezhi125@users.noreply.github.com>
2020-09-23 10:40:10 +0800

    Add files via upload

---
+++ UItextview UIimageviiew/UItextview UIimageviiew/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  UItextview UIimageviiew
+//
+//  Created by student on 2020/9/21.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let img = UIImage(named: "quanyecha"）
+            self.view.addSubview
+    }
+
+
+}
+
