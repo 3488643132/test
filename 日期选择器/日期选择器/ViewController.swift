329bd5d53818531a9982147e3adcabeaa07e7a0b
Kdz <64460590+Kongdezhi125@users.noreply.github.com>
2020-09-28 10:59:21 +0800

    Add files via upload

---
+++ 日期选择器/日期选择器/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  日期选择器
+//
+//  Created by student on 2020/9/28.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var data: UIDatePicker!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
