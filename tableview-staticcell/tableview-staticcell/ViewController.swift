ff4a609e7873e91ab8f1fa57700f229e4cb90786
Kdz <64460590+Kongdezhi125@users.noreply.github.com>
2020-11-06 11:25:59 +0800

    静态单元格

---
+++ tableview-staticcell/tableview-staticcell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableview-staticcell
+//
+//  Created by student on 2020/11/6.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
