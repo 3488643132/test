13660ee26c61fa20e5b9b20c1cae0f6a93320bd2
Kdz <64460590+Kongdezhi125@users.noreply.github.com>
2020-11-25 09:25:11 +0800

    纯代码方式构建标签控制器

---
+++ 标签控制器-代码/标签控制器-代码/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  标签控制器-代码
+//
+//  Created by student on 2020/11/25.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
