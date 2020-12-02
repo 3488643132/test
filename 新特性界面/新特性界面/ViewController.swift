caaacf3a77614b64f6450f687a48dc863967d48e
Kdz <64460590+Kongdezhi125@users.noreply.github.com>
2020-12-02 09:49:32 +0800

    数据存储-新特性界面

---
+++ 新特性界面/新特性界面/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  新特性界面
+//
+//  Created by student on 2020/12/2.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+    }
+
+
+}
+
