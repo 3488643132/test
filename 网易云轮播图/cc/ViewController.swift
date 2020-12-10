1281048d72f9586474d25f399628ad49f9fe5c54
Kdz <64460590+Kongdezhi125@users.noreply.github.com>
2020-12-10 13:58:02 +0800

    网易云轮播图，tableview

--- 网易云轮播图/cc/ViewController.swift
+++ 网易云轮播图/cc/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
     
     @IBOutlet weak var page: UIPageControl!
     @IBOutlet weak var scrollview: UIScrollView!
@@ -20,17 +21,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         for (index,imagename) in imagename.enumerated() {
             let imagephoto = UIImageView(image: UIImage(named: imagename))
-            imagephoto.frame = CGRect(x: scrollview.frame.size.width * CGFloat(index), y: 0, width: scrollview.frame.size.width, height: 420)
+            imagephoto.frame = CGRect(x: scrollview.frame.size.width * CGFloat(index), y: 0, width: scrollview.frame.size.width, height: scrollview.frame.size.height)
             
             scrollview.addSubview(imagephoto)
         }
-        
-        scrollview.contentSize = CGSize(width: CGFloat(imagename.count)*scrollview.frame.size.width, height: 420)
+        scrollview.isPagingEnabled = true
+        scrollview.bounces = false
+        scrollview.contentSize = CGSize(width: CGFloat(imagename.count)*scrollview.frame.size.width, height: scrollview.frame.size.height)
         scrollview.delegate = self
+        
     }
     
     @IBAction func page2(_ sender: UIPageControl) {
         let count = sender.currentPage
+        
         let offsetx = scrollview.frame.width * CGFloat(count)
         scrollview.setContentOffset(CGPoint(x: offsetx, y: 0), animated: true)
         
@@ -38,6 +42,7 @@ class ViewController: UIViewController {
     }
 }
 extension ViewController:UIScrollViewDelegate{
+    
     public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         let offset = scrollView.contentOffset
         let count = offset.x / scrollView.frame.width
@@ -47,4 +52,34 @@ extension ViewController:UIScrollViewDelegate{
     }
     
 }
+var num = ["1","2","3"]
+var people = ["qq","cc","vv",]
+var time = ["11.11","11.12","11.13"]
 
+
+extension ViewController:UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "aaa") as! musicTableViewCell
+        cell.labalone.text = num[indexPath.row]
+        cell.labaltwo.text = people[indexPath.row]
+        cell.labalthree.text = time[indexPath.row]
+        return cell
+        
+    }
+    
+    
+}
+extension ViewController:UITableViewDelegate{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0
+    }
+    
+    
+}
---
+++ 网易云轮播图/cc/musicTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  musicTableViewCell.swift
+//  cc
+//
+//  Created by student on 2020/12/10.
+//  Copyright © 2020 student. All rights reserved.
+//
+
+import UIKit
+
+class musicTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var labalthree: UILabel!
+    @IBOutlet weak var labaltwo: UILabel!
+    @IBOutlet weak var labalone: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
